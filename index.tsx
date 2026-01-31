8ed0205d405917511b715101cba835bbfc345459
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2026-02-01 00:43:15 +0400

    Initialize GoogleGenerativeAI with API key
    Add API key and initialize GoogleGenerativeAI instance

--- index.tsx
+++ index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const API_KEY = import.meta.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");

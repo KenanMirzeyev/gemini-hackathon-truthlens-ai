508e9536a1a526adec4d36980033cf30edb40717
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2026-02-01 00:48:35 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(API_KEY);
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
